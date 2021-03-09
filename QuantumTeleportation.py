606455f91a6e1bbd9bb79ec30121befb20d58b9e
Marco Armenta <drmarcoarmenta@gmail.com>
2021-03-09 16:56:06 -0500

    BV-algorithm (#1)
    * BV-algorithm
    
    * fixed a message
    
    Co-authored-by: Marco Armenta <marco.antonio.armenta.armenta@usherbrooke.ca>

--- QuantumTeleportation.py
+++ QuantumTeleportation.py
@@ -1,9 +1,9 @@
-import numpy as np
 import matplotlib.pyplot as plt
 from qiskit.visualization import plot_histogram
 from qiskit import *
 
 if __name__ == "__main__":
+	plot_circuit = input("Do you want to plot the circuit? No=0, Yes=1: ")
 	teleport = QuantumCircuit(3,3)
 	teleport.x(0) #initialize q0=|1> for teleportation
 	teleport.h(1) #entangle qubits q1 and q2
@@ -14,14 +14,16 @@ if __name__ == "__main__":
 	teleport.cx(1,2)
 	teleport.cz(0,2)
 	teleport.measure(2,2)
-	teleport.draw(output='mpl')
+
+	if int(plot_circuit):
+		teleport.draw(output = 'mpl')
 
 	print('Qubit q0 after the X gate is in state |1>'\
 		' which will be teleported to qubit q2.')
 
 	hist = execute(teleport, backend = Aer.get_backend('qasm_simulator'), 
 			shots = 256).result().get_counts()
-	print('We can see in the histogram that we only have results for states'\
+	print('We can see in the histogram that we only have results'\
 		' where qubit q2 is in state |1>. This confirms teleportation.')
 	plot_histogram(hist)
 	plt.show()
